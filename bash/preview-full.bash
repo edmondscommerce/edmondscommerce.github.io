bb3c6b474ca351c7106f8941b9839d3f849f9b4e
Ross Mitchell <ross@edmondscommerce.co.uk>
2014-01-27 12:32:39 +0000

    commiting the source

--- bash/preview-full.bash
+++ bash/preview-full.bash
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+set -e;
 bundle exec rake integrate;
 bundle exec rake generate;
 bundle exec rake preview;
--- bash/preview-latest-post.bash
+++ bash/preview-latest-post.bash
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+set -e;
 PATHTOLATEST=`ls -t source/_posts | head -1`;
 echo $PATHTOLATEST
 bundle exec rake isolate[$PATHTOLATEST];
