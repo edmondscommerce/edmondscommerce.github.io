74b4caf42888755d02083b1385a2124c0a6b3d59
joseph <joseph@edmondscommerce.co.uk>
2014-11-11 14:12:22 +0000

    commiting the source

--- bash/git-push-gen-deploy.bash
+++ bash/git-push-gen-deploy.bash
@@ -4,7 +4,7 @@ set -x;
 cd /opt/Projects/edmondscommerce.github.io/
 git checkout source;
 if [ ! -d _deploy/.git ]; then
-  rake setup_github_pages;
+  bundle exec rake setup_github_pages;
 fi
 bundle exec rake integrate
 git add -A
